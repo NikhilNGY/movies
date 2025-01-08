d0443ffc6af75b875cd8108bbb030feab630b070
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-01-08 13:19:18 +0530

    Update custom_dl.py

--- Jisshu/util/custom_dl.py
+++ Jisshu/util/custom_dl.py
@@ -25,8 +25,8 @@ class ByteStreamer:
             generate_media_session: returns the media session for the DC that contains the media file.
             yield_file: yield a file from telegram servers for streaming.
             
-        This is a modified version of the <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/telegram/utils/custom_download.py>
-        Thanks to Eyaadh <https://github.com/eyaadh>
+        This is a modified version of the <https://t.me/KR_Picture>
+        Thanks to Eyaadh <https://t.me/KR_Picture>
         """
         self.clean_timer = 30 * 60
         self.client: Client = client
@@ -172,8 +172,8 @@ class ByteStreamer:
     ) -> Union[str, None]:
         """
         Custom generator that yields the bytes of the media file.
-        Modded from <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/telegram/utils/custom_download.py#L20>
-        Thanks to Eyaadh <https://github.com/eyaadh>
+        Modded from <https://t.me/KR_Picture>
+        Thanks to Eyaadh <https://t.me/KR_Picture>
         """
         client = self.client
         work_loads[index] += 1
